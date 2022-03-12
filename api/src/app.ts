e8ffe6835d7c3f7832f3dba886aa6a3d19e5b3c6
jamespeccia <jamespeccia@outlook.com>
2022-03-12 00:52:30 -0600

    add user endpoint and env settings

---
+++ api/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import userRouter from './endpoints/user/express'
+
+const app = express();
+const port = 3000;
+
+app.use('/user', userRouter);
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}.`);
+});
+
---
+++ api/src/endpoints/user/read.ts
@@ -0,0 +1,5 @@
+const read = (req, res) => {
+    res.json({id: "abc123"})
+}
+
+export default read;
