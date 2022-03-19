3cca63118838da3e237f0390dcba64772080217b
jamespeccia <jamespeccia@outlook.com>
2022-03-18 21:13:19 -0500

    connect login form / add logging middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,6 +4,7 @@ import passport from 'passport'
 import session from 'express-session'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import morgan from 'morgan'
 
 import AuthRouter from 'endpoints/auth/express'
 import UserRouter from 'endpoints/user/express'
@@ -16,6 +17,7 @@ const port = 3030;
 
 mongoose.connect('mongodb://localhost:27017/auctionmadness');
 
+app.use(morgan(':method :status :url :response-time'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
