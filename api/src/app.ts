6d1cc5148bb18ef6b38d1bcd2ae12dc48e356159
jamespeccia <jamespeccia@outlook.com>
2022-03-18 21:43:36 -0500

    change db

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,7 +15,7 @@ import PassportStrategy from 'middleware/auth/passport'
 const app = express();
 const port = 3030;
 
-mongoose.connect('mongodb+srv://james:wYRsm3eeHhenzJm7@cluster0.tucj3.mongodb.net/Cluster0?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://james:wYRsm3eeHhenzJm7@cluster0.tucj3.mongodb.net/AuctionMadness?retryWrites=true&w=majority');
 
 app.use(morgan(':method :status :url :response-time'))
 app.use(bodyParser.json());
