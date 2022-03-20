0d64dc1eeaf3611e5e33a35124a64a9bbdc26fbb
jamespeccia <jamespeccia@outlook.com>
2022-03-19 20:59:26 -0500

    kinda fix docker

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,6 +5,7 @@ import session from 'express-session'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import enableWs from 'express-ws'
 
 import AuthRouter from 'endpoints/auth/express'
 import UserRouter from 'endpoints/user/express'
@@ -14,6 +15,8 @@ import PassportStrategy from 'middleware/auth/passport'
 
 const app = express();
 const port = 3030;
+
+enableWs(app);
 
 mongoose.connect('mongodb+srv://james:wYRsm3eeHhenzJm7@cluster0.tucj3.mongodb.net/AuctionMadness?retryWrites=true&w=majority');
 
@@ -29,6 +32,17 @@ PassportStrategy(passport);
 
 app.use('/auth', AuthRouter);
 app.use('/user', UserRouter);
+
+// @ts-ignore
+app.ws('/echo', (ws, req) => {
+  ws.on('message', msg => {
+    ws.send(msg)
+  })
+
+  ws.on('close', () => {
+    console.log('WebSocket was closed')
+  })
+})
 
 app.listen(port, () => {
   console.log(`Application is running on port ${port}.`);
